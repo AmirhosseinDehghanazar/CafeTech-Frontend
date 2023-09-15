86327e43ad0ecf65a482075e1429163ae2de524e
Amirhossein-Dehghanazar <a.dehghanazar@gmail.com>
2023-09-16 01:28:38 +0330

    Base Created

---
+++ CafeTech-Frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+      },
+    },
+  },
+  plugins: [],
+};
