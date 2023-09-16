6d97862d463248c705f378699c0a941bd941d714
Amirhossein-Dehghanazar <a.dehghanazar@gmail.com>
2023-09-16 19:13:02 +0330

    Router Configed !

---
+++ CafeTech-Frontend/src/layout/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <>
+      <p className="bg-cyan-300">awdawd</p>
+    </>
+  );
+};
+
+export default Main;
--- CafeTech-Frontend/tailwind.config.js
+++ CafeTech-Frontend/tailwind.config.js
@@ -4,10 +4,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#00040f",
-        secondary: "#00f6ff",
-        dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
+        primary: "#131318",
+        secondary: "#3F2BDE",
       },
     },
   },
