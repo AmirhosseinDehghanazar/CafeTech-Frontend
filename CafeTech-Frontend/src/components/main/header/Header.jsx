cd7c8e7a74cc6958cbde75b4c2ce3e0fcd01b1bf
mohamad amin <mohamadeaminn@gmail.com>
2023-09-17 19:48:59 +0400

    header big text + emojies are donr responsive

--- CafeTech-Frontend/src/components/main/header/Header.jsx
+++ CafeTech-Frontend/src/components/main/header/Header.jsx
@@ -1,24 +1,33 @@
 import React from "react";
+import "./header.css";
 
 const Header = () => {
   return (
     <div className="flex flex-col justify-center items-center">
       {/* big text */}
       <div>
-        <div className="relative">
-          <p className="text-6xl">
-            ما اینجاییم تا قدمی فراتر از {"{کد}"} برداریم
-          </p>{" "}
-          {/* fire */}
-          <div />
-          {/* sparks */}
-          <div />
+        <div className=" text-center text-white">
+          <div className="text-6xl header-big-text">
+            <span className="relative">
+              ما اینجاییم تا قدمی فراتر از {"{کد}"} برداریم
+              {/* fire */}
+              <div className="absolute bg-fire bg-img rounded-full">
+                <div className="bg-orange-spread flex mx-auto mt-6"></div>
+              </div>
+              {/* sparks */}
+              <div className="absolute bg-sparkle bg-img">
+                <div className="bg-orange-spread flex mx-auto mt-6"></div>
+              </div>
+            </span>
+          </div>
         </div>
       </div>
+
       {/* text */}
       <div>
         <p></p>
       </div>
+
       {/* buttons */}
       <div></div>
     </div>
