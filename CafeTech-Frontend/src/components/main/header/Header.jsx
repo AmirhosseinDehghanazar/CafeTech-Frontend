6177fa936be3c81feb3087c095aa67db4fea4c18
mohamad amin <mohamadeaminn@gmail.com>
2023-09-17 20:00:39 +0400

    second text for header in done

--- CafeTech-Frontend/src/components/main/header/Header.jsx
+++ CafeTech-Frontend/src/components/main/header/Header.jsx
@@ -5,27 +5,29 @@ const Header = () => {
   return (
     <div className="flex flex-col justify-center items-center">
       {/* big text */}
-      <div>
-        <div className=" text-center text-white">
-          <div className="text-6xl header-big-text">
-            <span className="relative">
-              ما اینجاییم تا قدمی فراتر از {"{کد}"} برداریم
-              {/* fire */}
-              <div className="absolute bg-fire bg-img rounded-full">
-                <div className="bg-orange-spread flex mx-auto mt-6"></div>
-              </div>
-              {/* sparks */}
-              <div className="absolute bg-sparkle bg-img">
-                <div className="bg-orange-spread flex mx-auto mt-6"></div>
-              </div>
-            </span>
-          </div>
+
+      <div className=" text-center text-white mb-12">
+        <div className="text-6xl header-big-text">
+          <span className="relative">
+            ما اینجاییم تا قدمی فراتر از {"{کد}"} برداریم
+            {/* fire */}
+            <div className="absolute bg-fire bg-img rounded-full">
+              <div className="bg-orange-spread flex mx-auto mt-6"></div>
+            </div>
+            {/* sparks */}
+            <div className="absolute bg-sparkle bg-img">
+              <div className="bg-orange-spread flex mx-auto mt-6"></div>
+            </div>
+          </span>
         </div>
       </div>
 
       {/* text */}
       <div>
-        <p></p>
+        <p className="text-xl text-center header-sec-text max-w-xl">
+          کافه تک، رسانه تخصصی و کامیونیتی برنامه نویسان و علاقه‌مندان به
+          تکنولوژی و برگزار کننده رویدادهای حضوری و آنلاین جذاب.
+        </p>
       </div>
 
       {/* buttons */}
