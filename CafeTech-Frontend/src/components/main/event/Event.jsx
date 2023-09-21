1d90fe35e11cf68c0ed28ae23e630edd38d40b85
mohamad amin <mohamadeaminn@gmail.com>
2023-09-21 15:09:28 +0400

    responsive event component

--- CafeTech-Frontend/src/components/main/event/Event.jsx
+++ CafeTech-Frontend/src/components/main/event/Event.jsx
@@ -2,10 +2,32 @@ import { format } from "date-fns-jalali";
 import "./event.css";
 import { eventsData } from "./eventsData";
 import Slider from "react-slick";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Event = () => {
+  const [slidesToShow, setSlidesToShow] = useState(1);
+
+  useEffect(() => {
+    window.addEventListener("load", () => {
+      if (window.innerWidth < 1024) {
+        setSlidesToShow(1);
+      } else if (window.innerWidth > 1024) {
+        setSlidesToShow(2);
+      }
+    });
+
+    window.addEventListener("resize", () => {
+      if (window.innerWidth < 1024) {
+        setSlidesToShow(1);
+      } else if (window.innerWidth > 1024) {
+        setSlidesToShow(2);
+      }
+    });
+  }, [window.innerWidth]);
+
   return (
-    <div>
+    <div className="mb-20">
       {/* top texts */}
       <div className="flex flex-col md:flex-row mb-6 gap-2 md:gap-0 items-center justify-center text-center md:justify-around lg:justify-between">
         <div className="text-[32px] lg:text-[36px] bg-blue-text-full text-white">
@@ -17,10 +39,15 @@ const Event = () => {
         </a>
       </div>
 
-      {/* all in:map data below and add slick above all */}
-      <Slider arrows={false} dots className="text-center">
+      {/* all in:map data below and add slick */}
+      <Slider
+        slidesToShow={slidesToShow}
+        arrows={false}
+        dots
+        className="text-center "
+      >
         {eventsData.map((e) => (
-          <div className="relative event_event-parent rounded-[25px] w-full max-w-2xl h-[368px] mb-32 md:mb-20">
+          <div className="relative event_event-parent rounded-[25px] max-w-[95%] h-[368px] mb-32">
             <div>
               <p></p>
             </div>
@@ -28,17 +55,17 @@ const Event = () => {
             <div className="event_eventdata w-[90%] border p-4 border-[#4B4B6A] rounded-[25px]">
               {/* title and isDone */}
               <div className="flex items-center justify-between">
-                <p className="text-[22px] font-bold text-white mb-2">
+                <p className="text-[22px] xl:text-[28px] font-bold text-white mb-2">
                   {e.title}
                 </p>
                 {e.done && (
-                  <div className="text-white/80 py-1 px-2 text-[14px] rounded-[27px] bg-[#4B4B6A]">
+                  <div className="text-white/80 py-1 px-2 text-[14px] xl:text-[18px] rounded-[27px] bg-[#4B4B6A]">
                     برگذار شده
                   </div>
                 )}
               </div>
 
-              <div className="text-[14px] flex flex-col gap-2">
+              <div className="text-[14px] xl:text-[18px] flex flex-col gap-2">
                 <div className="flex flex-col gap-1.5 md:flex-row">
                   {/* event time */}
                   <div className="flex gap-2 items-center">
