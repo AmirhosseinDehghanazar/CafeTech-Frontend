abf33810e35be5212f9525024c8ac13eabb64a4d
mohamad amin <mohamadeaminn@gmail.com>
2023-09-21 14:01:10 +0400

    slick dots css changed + add slick to events not responsive yet

--- CafeTech-Frontend/src/components/main/event/Event.jsx
+++ CafeTech-Frontend/src/components/main/event/Event.jsx
@@ -1,6 +1,7 @@
 import { format } from "date-fns-jalali";
 import "./event.css";
 import { eventsData } from "./eventsData";
+import Slider from "react-slick";
 
 const Event = () => {
   return (
@@ -17,48 +18,51 @@ const Event = () => {
       </div>
 
       {/* all in:map data below and add slick above all */}
-      <div className="relative event_event-parent rounded-[25px] h-[368px]">
-        <div>
-          <p></p>
-        </div>
-        {/* event data */}
-        <div className="event_eventdata w-[90%] border p-4 border-[#4B4B6A] rounded-[25px]">
-          {/* title and isDone */}
-          <div className="flex items-center justify-between">
-            <p className="text-[22px] font-bold text-white mb-2">
-              {eventsData[0].title}
-            </p>
-            {eventsData[0].done && (
-              <div className="text-white/80 py-1 px-2 text-[14px] rounded-[27px] bg-[#4B4B6A]">
-                برگذار شده
-              </div>
-            )}
-          </div>
-
-          <div className="text-[14px] flex flex-col gap-2">
-            <div className="flex flex-col gap-1.5 md:flex-row">
-              {/* event time */}
-              <div className="flex gap-2 items-center">
-                <div className="bg-clock w-[20px] h-[20px]" />
-                <p className="text-[#B3B3D0]">زمان: </p>
-                <p className="text-white/80">
-                  {format(eventsData[0].date, "dd MMMM")} ساعت{" "}
-                  {eventsData[0].start} الی {eventsData[0].end}
+      <Slider arrows={false} dots className="text-center">
+        {eventsData.map((e) => (
+          <div className="relative event_event-parent rounded-[25px] w-full max-w-2xl h-[368px] mb-32 md:mb-20">
+            <div>
+              <p></p>
+            </div>
+            {/* event data */}
+            <div className="event_eventdata w-[90%] border p-4 border-[#4B4B6A] rounded-[25px]">
+              {/* title and isDone */}
+              <div className="flex items-center justify-between">
+                <p className="text-[22px] font-bold text-white mb-2">
+                  {e.title}
                 </p>
+                {e.done && (
+                  <div className="text-white/80 py-1 px-2 text-[14px] rounded-[27px] bg-[#4B4B6A]">
+                    برگذار شده
+                  </div>
+                )}
               </div>
-              {/* event location */}
-              <div className="flex gap-2 items-center">
-                <div className="bg-location w-[21px] h-[20px]" />
-                <p className="text-[#B3B3D0]">مکان: </p>
-                <p className="text-white/80">{eventsData[0].location}</p>
+
+              <div className="text-[14px] flex flex-col gap-2">
+                <div className="flex flex-col gap-1.5 md:flex-row">
+                  {/* event time */}
+                  <div className="flex gap-2 items-center">
+                    <div className="bg-clock w-[20px] h-[20px]" />
+                    <p className="text-[#B3B3D0]">زمان: </p>
+                    <p className="text-white/80">
+                      {format(e.date, "dd MMMM")} ساعت {e.start} الی {e.end}
+                    </p>
+                  </div>
+                  {/* event location */}
+                  <div className="flex gap-2 items-center">
+                    <div className="bg-location w-[21px] h-[20px]" />
+                    <p className="text-[#B3B3D0]">مکان: </p>
+                    <p className="text-white/80">{e.location}</p>
+                  </div>
+                </div>
+                {/* event description */}
+                <p className="text-[#B3B3D0]">{e.description}</p>
               </div>
             </div>
-            {/* event description */}
-            <p className="text-[#B3B3D0]">{eventsData[0].description}</p>
+            {/* event data element end here */}
           </div>
-        </div>
-        {/* event data element end here */}
-      </div>
+        ))}
+      </Slider>
     </div>
   );
 };
