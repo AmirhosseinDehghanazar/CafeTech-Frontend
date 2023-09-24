3d48b729326edac4a1e5f4af0644943f97b2acfc
mohamad amin <mohamadeaminn@gmail.com>
2023-09-24 19:20:15 +0400

    footer text added

--- CafeTech-Frontend/src/components/footer/Footer.jsx
+++ CafeTech-Frontend/src/components/footer/Footer.jsx
@@ -2,7 +2,16 @@ import { useInView } from "react-intersection-observer";
 import "./footer.css";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="mb-10">
+      <div></div>
+      <p className="text-secondary text-lg font-semibold text-center">
+        تمامی حقوق این وبسایت متعلق به تیم{" "}
+        <span className="text-white/90">کافه تک</span> میباشد و هر گونه سوء
+        استفاده و کپی برداری از نام یا محتوای این سایت پیگرد قانونی دارد
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
