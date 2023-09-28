b0530678cc2b1868087d88a6dbdeb3caba9aa87f
mohamad amin <mohamadeaminn@gmail.com>
2023-09-28 22:51:25 +0400

    animation did not good so i deleted it

--- CafeTech-Frontend/src/components/main/guest/Guest.jsx
+++ CafeTech-Frontend/src/components/main/guest/Guest.jsx
@@ -9,16 +9,16 @@ const Guest = () => {
           <span>{"{تیم ها و مهمانان ویژه}"}</span>
         </p>
       </div>
-      <div className=" w-[10420px] -mx-6 md:-mx-10 lg:-mx-14 xl:-mx-20">
+      <div className="-mx-6 md:-mx-10 lg:-mx-14 xl:-mx-20">
         {/* people */}
-        <div className="flex animation-slide-toleft gap-3 overflow-auto mb-6">
+        <div className="flex gap-3 overflow-visible mb-6">
           {/* first one */}
           {data.people.map((person, i) => (
             <div
               key={i}
               className="min-w-[328px] md:min-w-[414px] bg-[#23232E] border border-[#4B4B6A] rounded-[15px] px-4 py-3"
             >
-              <div className="flex items-center gap-4 justify-start">
+              <div className="flex  items-center gap-4 justify-start">
                 <img
                   alt=""
                   src={person.img}
@@ -38,7 +38,7 @@ const Guest = () => {
         </div>
         {/* componies */}
 
-        <div className="flex animation-slide-toright gap-3 overflow-auto">
+        <div className="flex gap-3 overflow-visible">
           {data.compony.map((compony, i) => (
             <div
               key={i}
