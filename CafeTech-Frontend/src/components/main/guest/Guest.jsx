012019cd0c85f5ffc2c57b98f450e5d03e9edf00
mohamad amin <mohamadeaminn@gmail.com>
2023-09-28 22:28:46 +0400

    added componies to Guest component

--- CafeTech-Frontend/src/components/main/guest/Guest.jsx
+++ CafeTech-Frontend/src/components/main/guest/Guest.jsx
@@ -3,36 +3,67 @@ import data from "./data";
 
 const Guest = () => {
   return (
-    <div>
-      {/* people */}
-      <div className="flex w-[10420px] animation-guest-slide gap-3 overflow-auto -mx-6 md:-mx-10 lg:-mx-14 xl:-mx-20">
-        {/* first one */}
-        {data.people.map((person, i) => (
-          <div
-            key={i}
-            className="min-w-[328px] md:min-w-[414px] bg-[#23232E] border border-[#4B4B6A] rounded-[15px] px-4 py-3"
-          >
-            <div className="flex items-center gap-4 justify-start">
-              <img
-                alt=""
-                src={person.img}
-                className="w-[74px] h-[74px] rounded-[10px] md:w-[94px] md:h-[94px]"
-              />
-              <div className="flex flex-col items-start justify-center gap-2">
-                <p className="text-lg md:text-[22px] font-semibold text-white">
-                  {person.name}
-                </p>
-                <p className="text-[14px] md:text-lg text-secondary">
-                  {person.job}
-                </p>
+    <>
+      <div className="mb-10 font-bold m-auto text-[32px]">
+        <p className="bg-blue-text text-center">
+          <span>{"{تیم ها و مهمانان ویژه}"}</span>
+        </p>
+      </div>
+      <div className=" w-[10420px] -mx-6 md:-mx-10 lg:-mx-14 xl:-mx-20">
+        {/* people */}
+        <div className="flex animation-slide-toleft gap-3 overflow-auto mb-6">
+          {/* first one */}
+          {data.people.map((person, i) => (
+            <div
+              key={i}
+              className="min-w-[328px] md:min-w-[414px] bg-[#23232E] border border-[#4B4B6A] rounded-[15px] px-4 py-3"
+            >
+              <div className="flex items-center gap-4 justify-start">
+                <img
+                  alt=""
+                  src={person.img}
+                  className="w-[74px] h-[74px] rounded-[10px] md:w-[94px] md:h-[94px]"
+                />
+                <div className="flex flex-col items-start justify-center gap-2">
+                  <p className="text-lg md:text-[22px] font-semibold text-white">
+                    {person.name}
+                  </p>
+                  <p className="text-[14px] md:text-lg text-secondary">
+                    {person.job}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        {/* componies */}
+
+        <div className="flex animation-slide-toright gap-3 overflow-auto">
+          {data.compony.map((compony, i) => (
+            <div
+              key={i}
+              className="min-w-[328px] md:min-w-[414px] bg-[#23232E] border border-[#4B4B6A] rounded-[15px] px-4 py-3"
+            >
+              <div className="flex items-center gap-4 justify-start">
+                <img
+                  alt=""
+                  src={compony.img}
+                  className="w-[74px] h-[74px] rounded-[10px] md:w-[94px] md:h-[94px]"
+                />
+                <div className="flex flex-col items-start justify-center gap-2">
+                  <p className="text-lg md:text-[22px] font-semibold text-white">
+                    {compony.name}
+                  </p>
+                  <p className="text-[14px] md:text-lg text-secondary">
+                    {compony.job}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      {/* componies */}
-      <div></div>
-    </div>
+    </>
   );
 };
 
--- CafeTech-Frontend/src/components/main/guest/data.js
+++ CafeTech-Frontend/src/components/main/guest/data.js
@@ -1,8 +1,15 @@
+// people
 import bigdeli from "../../../assets/people/bigdeli.png";
 import ezat from "../../../assets/people/ezat.png";
 import bateni from "../../../assets/people/bateni.png";
 import shekari from "../../../assets/people/shekari.png";
 import shekari2 from "../../../assets/people/shekari2.png";
+// componies
+import robo from "../../../assets/compony/robo.png";
+import cs50 from "../../../assets/compony/cs50.png";
+import planet from "../../../assets/compony/planet.png";
+import digiNext from "../../../assets/compony/diginext.png";
+import mani from "../../../assets/compony/mani.png";
 
 // data for people in figma
 export default {
@@ -38,5 +45,36 @@ export default {
       img: shekari2,
     },
   ],
-  compony: [{}],
+  compony: [
+    {
+      name: "علی بیگدلی",
+      job: "بنیان‌گذار شرکت ارتباط هوشمند آریا",
+      img: bigdeli,
+    },
+    {
+      name: "Robo Epics",
+      job: " مسابقات هوش مصنوعی",
+      img: bigdeli,
+    },
+    {
+      name: "c50x iran",
+      job: "دوره برنامه نویسی هاروارد",
+      img: bigdeli,
+    },
+    {
+      name: "پلنت",
+      job: "مرکز شتابدهی و نوآوری بیمه ای ",
+      img: bigdeli,
+    },
+    {
+      name: "دیجی نکست",
+      job: "مرکز سرمایه گذاری گروه دیجیکالا",
+      img: bigdeli,
+    },
+    {
+      name: "پلتفرم مانی",
+      job: "پلتفرم تبدیل متن به عکس",
+      img: bigdeli,
+    },
+  ],
 };
